452fc9ddfbf2a28e70548f47d4395b68b22711f3
Miguel Hernandez <miguel@lap-m-lenov.com>
2022-08-20 09:28:09 -0500

    first version

---
+++ Level.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
+
+contract Web3ClubTourToken is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    
+    event Attest(address indexed to, uint256 indexed tokenId);
+    event Revoke(address indexed to, uint256 indexed tokenId);
+
+    constructor() ERC721("TestContract20220820Level", "202208200753Level") {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        //once it is minted we define everything that is needed to be stored in the json file. this file will be managed by us, images will be on IPFS.
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function burn(uint256 tokenId) external {
+        
+        require(ownerOf(tokenId) == msg.sender, "Only owner of the token cann bur it");
+        _burn(tokenId);
+    }
+
+    function revoke(uint256 tokenId) external onlyOwner {
+        //revoke after using a coupon
+        _burn(tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256) pure override internal {
+        require(from == address(0) || to == address(0), "Not allowed to transfer token");
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId) override internal {
+        if (from == address(0)) {
+            emit Attest(to, tokenId);
+        } else if (to == address(0)) {
+            emit Revoke(to, tokenId);
+        }
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
